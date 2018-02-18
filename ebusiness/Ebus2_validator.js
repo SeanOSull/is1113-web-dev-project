de0218a475f2a000845f3e632f2f0b0f56660db0
Sean O'Sullivan <117322633@umail.ucc.ie>
2018-02-18 18:06:39 +0000

    #4

--- ebusiness/Ebus2_validator.js
+++ ebusiness/Ebus2_validator.js
@@ -1,47 +1,28 @@
+
+
 /* global $ */
-
-
 
 function validateDetails(){
 
-    
-
-    var pin;
-
-    
+     var pin;
 
     pin = document.getElementById("user_pin").value;
 
-    if (pin == ""){
-
+    if (pin ==""){
         alert("Please enter your PIN");
-
     }
-
-    else if (String(pin).length < 4){
-
+    else if (String(pin).length<4){
         alert("Please make sure your PIN is accurate");
-
-    }
-
-    else{
-
-        enablebtnPurchase();
-
-    }
-
-    
-
+        }
+        else{
+            enablebtnPurchase();
+            }
 }
 
 function enablebtnPurchase(){
-
-    $('#btnPurchase').prop('disabled', false);
+    $('#btnPurchase').prop('disabled',false);
 
 }
-
 function disablebtnPurchase() {
-
-    $('#btnPurchase').prop('disabled', true);
-
+    $('#btnPurchase').prop('disabled',true);
 }
--- index.php
+++ index.php
@@ -1,2 +1,2 @@
-
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
 <?php include_once("homepage.html") ?>
