c813822e0dec0ea5c735cc0759701be2369706a3
Sean O'Sullivan <117322633@umail.ucc.ie>
2018-03-06 23:22:20 +0000

    #3

--- ebusiness/Ebus2_validator.js
+++ ebusiness/Ebus2_validator.js
@@ -1,30 +1,35 @@
-
-
 /* global $ */
 
 function validateDetails(){
 
-     var pin, NAME, EMAIL;
+     var pin, NAME, EMAIL, pass;
 
  pin = document.getElementById("user_pin").value;
  EMAIL = document.getElementById("EMAIL").value;
  NAME = document.getElementById("NAME").value;
+ pass = document.getElementById("pass").value; pass=0
+ 
+ 
    
     
     if (String(pin).length<4){
         alert("Please make sure your PIN is accurate");
         }
+        else pass=+1
    
     if (NAME==""){
                  alert("Please enter your name.")
              }
+             else pass=+1
  
           
     if (EMAIL==""){
                  alert("Please enter your Email.")
              }  
-  else
-   {
+             else pass=+1
+  
+  if ((pass==3)){ 
+ 
                    enablebtnPurchase();
               }
           
