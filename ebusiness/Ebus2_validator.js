cb7eeac5e40d72a85f111b992bb0ebc53746f48c
Sean O'Sullivan <117322633@umail.ucc.ie>
2018-03-09 10:26:10 +0000

    Final #1

--- ebusiness/Ebus2_validator.js
+++ ebusiness/Ebus2_validator.js
@@ -2,41 +2,32 @@
 
 function validateDetails(){
 
-     var pin, NAME, EMAIL, pass;
-    
-    
+     var pin, NAME, EMAIL;
 
  pin = document.getElementById("user_pin").value;
  EMAIL = document.getElementById("EMAIL").value;
  NAME = document.getElementById("NAME").value;
- pass = document.getElementById("pass").value;  pass=0;
- 
  
    
     
     if (String(pin).length<4){
         alert("Please make sure your PIN is accurate");
         }
-        else {pass+=1;
-        }
+   
     if (NAME==""){
-                 alert("Please enter your name.");
+                 alert("Please enter your name.")
              }
-             else{ pass+=1;
-             }
+ 
           
     if (EMAIL==""){
-                 alert("Please enter your Email.");
+                 alert("Please enter your Email.")
              }  
-           
-             else { pass+=1
-             }
-      
-      if ((pass==3))
+  else
+   {
                    enablebtnPurchase();
               }
-              
-
+          
+            
   
            
 
@@ -45,6 +36,7 @@ function enablebtnPurchase(){
 
 }
 
-function disablebtnPurchase(){
-    $('#btnPurchase').prop('disabled',true);
+function disablebtnPurchase() {
+    $('#btnPurchase').prop('disabled',true)
+}
 }
